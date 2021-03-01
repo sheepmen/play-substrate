afa30e1ecaf6044d7465c4a4293b06365a4e44b5
sheep3 <747033643@qq.com>
2021-03-01 22:39:23 +0800

    dev: 生成chain spec文件

--- chain_spec/sub_key_gen.sh
+++ chain_spec/sub_key_gen.sh
@@ -1,4 +1,7 @@
-subkey inspect "$SECRET" > output/inspect.output
+subkey generate > output/inspect.output
+subkey generate >> output/inspect.output
+subkey generate >> output/inspect.output
+subkey generate >> output/inspect.output
 for i in 1 2 3 4; do for j in stash controller; do subkey inspect "$SECRET//$i//$j"; done; done > output/controller.output
 for i in 1 2 3 4; do for j in babe; do subkey inspect --scheme sr25519 "$SECRET//$i//$j"; done; done > output/babe.output
 for i in 1 2 3 4; do for j in grandpa; do subkey inspect --scheme ed25519 "$SECRET//$i//$j"; done; done	> output/grandpa.output
